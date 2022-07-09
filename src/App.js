4acda2f9e83cd7420513e5da8676585751eaef4f
Jackey Yang <jyang@castlepointanime.com>
2022-07-08 22:38:05 -0500

    used <Routes> which fixed reload bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // Dependency imports
 import "./styles/App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Component imports
 import Header from "./components/Header";
@@ -16,44 +16,25 @@ import Privacy from "./pages/Privacy";
 import Categoryentityidpage from "./pages/Categoryentityidpage";
 import Productpage from "./pages/Productpage";
 import Payment from "./pages/Payment";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/payment">
-            <Payment />
-          </Route>
-          <Route path="/Categoryentityidpage">
-            <Categoryentityidpage />
-          </Route>
-          <Route path="/Productpage">
-            <Productpage />
-          </Route>
-          <Route path="/privacy">
-            <Privacy />
-          </Route>
-          <Route path="/terms">
-            <Terms />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/category">
-            <Categorypage />
-          </Route>
-          <Route index path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/Payment" element={<Payment />}></Route>
+        <Route path="/Payment" element={<Categoryentityidpage />}></Route>
+        <Route path="/Payment" element={<Productpage />}></Route>
+        <Route path="/privacy" element={<Privacy />}></Route>
+        <Route path="/terms" element={<Terms />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/category" element={<Categorypage />}></Route>
+        <Route index path="/" element={<Home />}></Route>
+      </Routes>
       <Pagebottom />
-    </Router>
+    </BrowserRouter>
   );
 }
 
