2ec4c3bdf3d216111cb8b079f91caa2852461571
Mohit Mummon <mxkumar@protonmail.com>
2023-08-25 12:47:41 -0700

    Optimize charts styling in Dashboard for Mobile
    Removed uneccesary css from Unchurch.css

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,51 +1,46 @@
+import { Link } from 'react-router-dom';
 import Layout from '../Layout';
 import DashDiagramLine from './tabs/DashDiagramLine';
 import DashDiagramPie from './tabs/DashDiagramPie';
-import DashPartOne from './tabs/DashPartOne';
 import DashPartTwo from './tabs/DashPartTwo';
 
 export default function Dashbaord() {
 	return (
 		<>
 			<Layout>
-				<section>
-					<div className='DashPartOne'>
-						<DashPartOne />
-					</div>
+				<section className='max-w-6xl mx-auto'>
+					<header className='flex justify-between mt-8 mb-3 px-2'>
+						<h1 className='text-3xl font-bold'>DashBoard</h1>
+						<Link to='/admin' className='hidden sm:block'>
+							<button type='button' className='btn bg-qteal w-[130px]'>
+								<span className='text-white'>Admin</span>
+							</button>
+						</Link>
+					</header>
 
 					<hr />
 
-					<div className='DashDiagram flex justify-content-center my-4'>
-						<div className='col-lg-5'>
+					<div className='flex justify-center flex-col-reverse gap-4 my-4 sm:flex-row'>
+						<div className='p-4 sm:w-1/2'>
 							<DashDiagramLine />
 						</div>
 
-						<div className='col-lg-5'>
+						<div className='sm:w-1/2'>
 							<DashDiagramPie />
 						</div>
 					</div>
 
 					<hr />
 
-					<div className='DashPartOne'>
-						<div>Top Product</div>
-					</div>
+					<header className='mt-8 mb-3 px-2'>
+						<h1 className='text-3xl font-bold'>Top Selling Products</h1>
+					</header>
 
 					<div className='DashPartTwo'>
 						<DashPartTwo />
 					</div>
 
 					<hr />
-
-					{/* <div className="DashPartOne">
-            <div>Buy it Again</div>
-          </div>
-
-          <div className="DashPartThree">
-            <DashPartThree />
-          </div>
-
-          <hr /> */}
 				</section>
 			</Layout>
 		</>
